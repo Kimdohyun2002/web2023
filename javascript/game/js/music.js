0737a22fa20c5e56734c35c2a9f4967231ca2e68
Kimdohyun2002 <kshkmn1@gmail.com>
2023-04-25 18:56:23 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name : "1. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view01.png",
+        audio: "music_audio01"
+    }, {
+        name : "2. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view02.png",
+        audio: "music_audio02"
+    }, {
+        name : "3. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view03.png",
+        audio: "music_audio03"
+    }, {
+        name : "4. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view04.png",
+        audio: "music_audio04"
+    }, {
+        name : "5. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view05.png",
+        audio: "music_audio05"
+    }, {
+        name : "6. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view06.png",
+        audio: "music_audio06"
+    }, {
+        name : "7. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view07.png",
+        audio: "music_audio07   "
+    }, {
+        name : "8. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view08.png",
+        audio: "music_audio08"
+    }, {
+        name : "9. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view09.png",
+        audio: "music_audio09"
+    },{
+        name : "10. 저리가라",
+        artist : "아티스트이름",
+        img: "music_view010.png",
+        audio: "music_audio01   0"
+    }
+]
